ce392c65931239c65f50046cb0c3fda6e91a0281
Simon Stein <spstein@willamette.edu>
2023-03-03 09:30:50 -0800

    UI updates

--- JournalingAppSP/ContentView.swift
+++ JournalingAppSP/ContentView.swift
@@ -15,40 +15,82 @@ struct ContentView: View {
         NavigationView {
             VStack {
                 Text(getCurrentDate())
-                    .offset(y: -50)
+                    .offset(y: -40)
                 Text("Rose, Bud, Thorn")
-                    .font(.title)
-                    .offset(y: -50)
+                    .offset(y: -30)
+                    .font(.system(size: 40))
                 Text("A Mindful Way to Reflect")
                     .font(.subheadline)
-                    .offset(y: -50)
+                    .offset(y: -20)
                 
                 ScrollView(.horizontal) {
                     HStack(spacing: 20) {
                         NavigationLink(destination: Rose()) {
                             Rectangle()
                                 .frame(width: 300, height: 500)
-                            //.background(Color(red: 250 / 255, green: 242 / 255, blue: 242 / 255))
                                 .foregroundColor(Color(red: 0.9803921568627451, green: 0.9254901960784314, blue: 0.8941176470588236))
                                 .cornerRadius(10)
-                                .shadow(color: Color.black.opacity(0.5), radius: 7)
+                                //.shadow(color: Color.black.opacity(0.5), radius: 7)
+                                .overlay(
+                                        VStack {
+                                            Text("ROSE")
+                                            Text("A highlight, success, small win, or something positive that happened today.")
+                                                .foregroundColor(Color(red: 0.41568627450980394, green: 0.4117647058823529, blue: 0.4117647058823529))
+                                                .offset(y: 40)
+                                            Spacer()
+                                        }
+                                            .offset(y: 30)
+                                            .padding()
+                                            .foregroundColor(.black)
+                                            .font(.system(size: 30))
+                                        
+                                )
                         }
                         NavigationLink(destination: Bud()) {
                             Rectangle()
                                 .frame(width: 300, height: 500)
                                 .foregroundColor(Color(red: 0.9294117647058824, green: 0.9568627450980393, blue: 0.8862745098039215))
                                 .cornerRadius(10)
-                                .shadow(color: Color.black.opacity(0.5), radius: 7)
+                                //.shadow(color: Color.black.opacity(0.5), radius: 7)
+                                .overlay(
+                                        VStack {
+                                            Text("BUD")
+                                            Text("A challenge you experienced or something you can use more support with.")
+                                                .foregroundColor(Color(red: 0.41568627450980394, green: 0.4117647058823529, blue: 0.4117647058823529))
+                                                .offset(y: 40)
+                                            Spacer()
+                                        }
+                                            .offset(y: 30)
+                                            .padding()
+                                            .foregroundColor(.black)
+                                            .font(.system(size: 30))
+                                        
+                                )
                         }
                         NavigationLink(destination: Thorn()) {
                             Rectangle()
                                 .frame(width: 300, height: 500)
                                 .foregroundColor(Color(red: 0.9176470588235294, green: 0.9372549019607843, blue: 0.9686274509803922))
                                 .cornerRadius(10)
-                                .shadow(color: Color.black.opacity(0.5), radius: 7)
+                                //.shadow(color: Color.black.opacity(0.5), radius: 7)
+                                .overlay(
+                                        VStack {
+                                            Text("THORN")
+                                            Text("New ideas that have blossomed or something you are looking forward to knowing more about or experiencing.")
+                                                .foregroundColor(Color(red: 0.41568627450980394, green: 0.4117647058823529, blue: 0.4117647058823529))
+                                                .offset(y: 40)
+                                            Spacer()
+                                        }
+                                            .offset(y: 30)
+                                            .padding()
+                                            .foregroundColor(.black)
+                                            .font(.system(size: 30))
+                                        
+                                )
                         }
                     }
                 }
+                .padding(20)
             }
         }
     }
--- JournalingAppSP/Rose.swift
+++ JournalingAppSP/Rose.swift
@@ -4,16 +4,22 @@ import Foundation
 struct Rose: View {
     @State var address = ""
         var body: some View {
-            TextField("Describe a highlight, success, small win, or something positive that happened today.", text: $address, axis: .vertical)
-                .lineLimit(5...10)
-                .font(.system(size: 30))
-                .padding()
-            
+            VStack() {
+                TextField("Describe a highlight, success, small win, or something positive that happened today.", text: $address, axis: .vertical)
+                    .lineLimit(5...100)
+                    .font(.system(size: 30))
+                    .padding()
+                    .lineSpacing(10)
+                Spacer()
+                Image(systemName: "mic.circle.fill")
+                    .offset(y: -20)
+                    .font(.system(size: 60))
+                    .foregroundColor(.black)
+            }
+            .offset(y: 40)
+            .background(Color(red: 0.9803921568627451, green: 0.9254901960784314, blue: 0.8941176470588236))
         }
 }
-
-
-
 struct Rose_Previews: PreviewProvider {
     static var previews: some View {
         Rose()
