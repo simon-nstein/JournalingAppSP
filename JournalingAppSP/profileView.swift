8a5bfd2aefc50d605c9f0fac907756b36f620268
Paul McSlarrow <pmcslarrow@icloud.com>
2023-03-15 14:15:45 -0700

    UI Update (profile view)

--- JournalingAppSP/profileView.swift
+++ JournalingAppSP/profileView.swift
@@ -15,34 +15,97 @@ struct profileView: View {
         VStack {
             
             // User picture, name, and email
-            HStack {
-                UserImage(urlString: userProfile.picture)
-                    .padding()
-                
-                Spacer()
             
-                VStack {
-                    HStack {
-                        Spacer()
-                        TextView(
-                            text: userProfile.email,
-                            fontSize: CustomFontSize.standardFontSize,
-                            offset: 0,
-                            fontType: "Poppins-regular"
-                        )
-                    }
-                    
-                    
-                }.padding()
-            } //Hstack
+            UserImage(urlString: userProfile.picture)
+                .padding()
+            
+            Spacer()
+        
+            VStack {
+                TextView(
+                    text: userProfile.email,
+                    fontSize: 22,
+                    offset: 0,
+                    fontType: "Poppins-SemiBold"
+                )
+                TextView(
+                    text: "Joined in 2020",
+                    fontSize: 14,
+                    offset: 0,
+                    fontType: "Poppins-Light"
+                )
+            }
             Divider()
             Spacer()
             
             
             // User Data
             ScrollView {
-                Text("Fill with user data")
+                
+                BarView(
+                    image: "flame",
+                    text: "Current Streak",
+                    statistic: "3 days"
+                )
+                Divider()
+                
+                BarView(
+                    image: "flame",
+                    text: "Longest Streak",
+                    statistic: "9 days"
+                )
+                Divider()
+                
+                BarView(
+                    image: "flame",
+                    text: "This Week",
+                    statistic: "2/7 Days"
+                )
+                Divider()
+                
+                BarView(
+                    image: "flame",
+                    text: "This Month",
+                    statistic: "10/30 Days"
+                )
+                Divider()
+                
+                BarView(
+                    image: "flame",
+                    text: "This Year",
+                    statistic: "50/365 days"
+                )
+                Divider()
+
             }
+        }
+    }
+}
+
+struct BarView: View {
+    let image: String
+    let text: String
+    let statistic: String
+    
+    let darkBlue = CustomColor.darkBlue
+    let textColor = CustomColor.TextColor
+    
+    var body: some View {
+        HStack {
+            Image(systemName: image)
+                .foregroundColor(darkBlue)
+                .padding(.horizontal)
+        
+            Text(text)
+                .font(Font.custom("Poppins-SemiBold", size: 20))
+                .foregroundColor(textColor)
+            
+            
+            Spacer()
+            
+            Text(statistic)
+                .font(Font.custom("Poppins-Regular", size: CustomFontSize.standardFontSize))
+                .padding(.horizontal)
         }
     }
 }
